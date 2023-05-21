ae7169deb96430d1b219cd5aa05e1d16aa33b7fd
Lucas Macedo <lucasoad399@hotmail.com>
2023-05-21 19:41:19 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -40,6 +40,7 @@ public class ChessMatch {
 	}
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) throw new ChessException("Não existe peça na posição de origem");
+		if(!board.piece(position).isThereAnyPossibleMove()) throw new ChessException ("Não existem movimentos disponíveis para a peça escolhida");
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
